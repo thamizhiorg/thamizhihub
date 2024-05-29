d128e698eaddd1530d68ae4b63e5016c7fa514e3
tarfwrk <tarfwrk@gmail.com>
2024-05-29 04:37:53 +0000

    prefix 2

--- server/api/files/upload.post.ts
+++ server/api/files/upload.post.ts
@@ -1,18 +1,20 @@
 
 export default eventHandler(async (event) => {
-  const form = await readFormData(event)
-  const file = form.get('file') as File
-  const filename = form.get('filename') as string
-  const author = form.get('author') as string
-  const mail = form.get('mail') as string
-  const prefix = form.get('mail') as string
+  const form = await readFormData(event);
+  const file = form.get('file') as File;
+  const filename = form.get('filename') as string;
+  const author = form.get('author') as string;
+  const mail = form.get('mail') as string;
+  const prefix = form.get('prefix') as string;
+
 
   // Allow all file types
-  const allowedTypes = ['*/*']
+  const allowedTypes = ['*/*'];
 
   // Ensure file size limit
-  ensureBlob(file, { maxSize: '100MB' })
+  ensureBlob(file, { maxSize: '100MB' });
 
-  // Upload file to blob storage using the custom file name
-  return hubBlob().put(filename, file, { authorname: author, email: mail  }, { addPrefix: prefix })
-})
+
+  // Upload file to blob storage using the custom file name with prefix
+  return hubBlob().put(`prefix/${filename}`, file, { authorname: author, email: mail });
+});
