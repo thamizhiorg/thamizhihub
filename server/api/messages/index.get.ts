2acdc065906c821a92a71571d3bea6701adb50ea
tarfwrk <tarfwrk@gmail.com>
2024-05-30 07:55:53 +0000

    table aa

--- server/api/messages/index.get.ts
+++ server/api/messages/index.get.ts
@@ -1,9 +1,9 @@
 export default eventHandler(async () => {
   const db = hubDatabase()
 
-await db.exec('CREATE TABLE IF NOT EXISTS contents (id INTEGER PRIMARY KEY, category TEXT, title TEXT, post TEXT, notes TEXT, url TEXT, links TEXT, date TEXT, thumbnail TEXT, type TEXT, video TEXT, audio TEXT, doc TEXT, email TEXT, profile TEXT, cardtitle TEXT, created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)')
+await db.exec('CREATE TABLE IF NOT EXISTS aa (id INTEGER PRIMARY KEY, email TEXT, username TEXT, name TEXT, description TEXT, status TEXT, about TEXT, skills TEXT, works TEXT, social TEXT, store TEXT, video TEXT, doc TEXT, image TEXT, phone TEXT, created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)')
 
-  const { results } = await db.prepare('SELECT * FROM contents ORDER BY created_at DESC').all()
+  const { results } = await db.prepare('SELECT * FROM aa ORDER BY created_at DESC').all()
 
   return results
 })
