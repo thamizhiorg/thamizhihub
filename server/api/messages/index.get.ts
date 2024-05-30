28487dab28ba0d9c6afa47d9ba3355cd1c06466a
tarfwrk <tarfwrk@gmail.com>
2024-05-30 23:45:41 +0000

    Items 1

--- server/api/messages/index.get.ts
+++ server/api/messages/index.get.ts
@@ -1,42 +1,42 @@
 export default eventHandler(async () => {
-  const db = hubDatabase();
+  const db = hubDatabase()
 
   await db.exec(`
-    CREATE TABLE IF NOT EXISTS Items (
-      id INTEGER PRIMARY KEY,
-      reorder INTEGER,
-      stock INTEGER,
-      weight INTEGER,
-      lastupdate DATETIME,
-      lastsync DATETIME,
-      storeid INTEGER,
-      cprice INTEGER,
-      upc INTEGER,
-      isbn INTEGER,
-      sprice INTEGER,
-      ean INTEGER,
-      color TEXT,
-      material TEXT,
-      manufacturer TEXT,
-      brand TEXT,
-      mpn TEXT,
-      imglist TEXT,
-      name TEXT,
-      category TEXT,
-      type TEXT,
-      sku TEXT,
-      unit TEXT,
-      location TEXT,
-      menu TEXT,
-      primaryimg TEXT,
-      desc TEXT,
-      grp TEXT,
-      dimen TEXT,
-      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    )
-  `);
+  CREATE TABLE IF NOT EXISTS Items (
+    id INTEGER PRIMARY KEY,
+    reorder INTEGER,
+    stock INTEGER,
+    weight INTEGER,
+    lastupdate DATETIME,
+    lastsync DATETIME,
+    storeid INTEGER,
+    cprice INTEGER,
+    upc INTEGER,
+    isbn INTEGER,
+    sprice INTEGER,
+    ean INTEGER,
+    color TEXT,
+    material TEXT,
+    manufacturer TEXT,
+    brand TEXT,
+    mpn TEXT,
+    imglist TEXT,
+    name TEXT,
+    category TEXT,
+    type TEXT,
+    sku TEXT,
+    unit TEXT,
+    location TEXT,
+    menu TEXT,
+    primaryimg TEXT,
+    desc TEXT,
+    grp TEXT,
+    dimen TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  )
+`);
 
-  const { results } = await db.prepare('SELECT * FROM Items ORDER BY created_at DESC').all();
+  const { results } = await db.prepare('SELECT * FROM Items ORDER BY created_at DESC').all()
 
-  return results;
-});
+  return results
+})
