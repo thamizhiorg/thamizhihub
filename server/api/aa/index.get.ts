425419dca238bbdb5439070981e6baba8e0f23f7
tarfwrk <tarfwrk@gmail.com>
2024-05-30 09:12:12 +0000

    aa 2

--- server/api/aa/index.get.ts
+++ server/api/aa/index.get.ts
@@ -1,7 +1,7 @@
 export default eventHandler(async () => {
   const db = hubDatabase()
 
-  const { results } = await db.prepare('SELECT * FROM contents ORDER BY created_at DESC').all()
+  const { results } = await db.prepare('SELECT * FROM aa ORDER BY created_at DESC').all()
 
   return results
 })
