dbc9af98eb66d725069fbd003718426391056e2a
tarfwrk <tarfwrk@gmail.com>
2024-05-31 03:10:13 +0000

    item up 1

--- server/api/Items/index.post.ts
+++ server/api/Items/index.post.ts
@@ -1,5 +1,6 @@
 export default eventHandler(async (event) => {
   const {
+    id,
     reorder,
     stock,
     weight,
@@ -28,13 +29,14 @@ export default eventHandler(async (event) => {
     desc,
     grp,
     dimen,
-    created_at
+    created_at,
   } = await readBody(event);
   const db = hubDatabase();
 
   await db
     .prepare(`
       INSERT INTO Items (
+        id,
         reorder,
         stock,
         weight,
@@ -65,12 +67,13 @@ export default eventHandler(async (event) => {
         dimen,
         created_at
       ) VALUES (
-        ?1, ?2, ?3, ?4, ?5, ?6, ?7, ?8, ?9, ?10,
-        ?11, ?12, ?13, ?14, ?15, ?16, ?17, ?18, ?19, ?20,
-        ?21, ?22, ?23, ?24, ?25, ?26, ?27, ?28, ?29, ?30
+        ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,
+        ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,
+        ?, ?, ?, ?, ?, ?, ?, ?, ?, ?
       )
     `)
     .bind(
+      id,
       reorder,
       stock,
       weight,
