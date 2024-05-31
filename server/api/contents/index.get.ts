d33e34d701ebccb82a450f7cf341a167181e226d
tarfwrk <tarfwrk@gmail.com>
2024-05-31 20:13:07 +0000

    contents

--- server/api/contents/index.get.ts
+++
@@ -1,7 +0,0 @@
-export default eventHandler(async () => {
-  const db = hubDatabase()
-
-  const { results } = await db.prepare('SELECT * FROM contents ORDER BY created_at DESC').all()
-
-  return results
-})
