f8c1c576aed189179c432545fc987e5d9c6dc7f9
tarfwrk <tarfwrk@gmail.com>
2024-05-31 00:06:42 +0000

    items 5

--- server/api/messages/index.get.ts
+++ server/api/messages/index.get.ts
@@ -1,40 +1,7 @@
 export default eventHandler(async () => {
   const db = hubDatabase()
 
-  await db.exec(`
-  CREATE TABLE IF NOT EXISTS Items (
-    id INTEGER PRIMARY KEY,
-    reorder INTEGER,
-    stock INTEGER,
-    weight INTEGER,
-    lastupdate TIMESTAMP,
-    lastsync TIMESTAMP,
-    storeid INTEGER,
-    cprice INTEGER,
-    upc INTEGER,
-    isbn INTEGER,
-    sprice INTEGER,
-    ean INTEGER,
-    color TEXT,
-    material TEXT,
-    manufacturer TEXT,
-    brand TEXT,
-    mpn TEXT,
-    imglist TEXT,
-    name TEXT,
-    category TEXT,
-    type TEXT,
-    sku TEXT,
-    unit TEXT,
-    location TEXT,
-    menu TEXT,
-    primaryimg TEXT,
-    desc TEXT,
-    grp TEXT,
-    dimen TEXT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-  )
-`);
+await db.exec('CREATE TABLE IF NOT EXISTS Items (id INTEGER PRIMARY KEY, email TEXT, username TEXT, name TEXT, description TEXT, status TEXT, about TEXT, skills TEXT, works TEXT, social TEXT, store TEXT, video TEXT, doc TEXT, image TEXT, phone TEXT, created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)')
 
   const { results } = await db.prepare('SELECT * FROM Items ORDER BY created_at DESC').all()
 
