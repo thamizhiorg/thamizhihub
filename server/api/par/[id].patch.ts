45346065d7c48e2117c8940c5f5deb71bdcb0b1f
tarfwrk <tarfwrk@gmail.com>
2024-06-01 08:06:56 +0000

    par 2

--- server/api/par/[id].patch.ts
+++ server/api/par/[id].patch.ts
@@ -1,16 +1,58 @@
 export default eventHandler(async (event) => {
-  const { type, plan, user, url, size, lastsync, secrets, pageid, title, views, analytics } = await readBody(event);
+  const {
+    type,
+    plan,
+    user,
+    url,
+    size,
+    lastsync,
+    secrets,
+    pageid,
+    title,
+    views,
+    analytics,
+    thumbnail,
+  } = await readBody(event);
   const id = getRouterParam(event, 'id');
   const db = hubDatabase();
 
   const result = await db
-    .prepare('UPDATE par SET type = ?, plan = ?, user = ?, url = ?, size = ?, lastsync = ?, secrets = ?, pageid = ?, title = ?, views = ?, analytics = ? WHERE id = ?')
-    .bind(type, plan, user, url, size, lastsync, secrets, pageid, title, views, analytics, id)
+    .prepare(`
+      UPDATE par SET
+        type = ?,
+        plan = ?,
+        user = ?,
+        url = ?,
+        size = ?,
+        lastsync = ?,
+        secrets = ?,
+        pageid = ?,
+        title = ?,
+        views = ?,
+        analytics = ?,
+        thumbnail = ?
+      WHERE id = ?
+    `)
+    .bind(
+      type,
+      plan,
+      user,
+      url,
+      size,
+      lastsync,
+      secrets,
+      pageid,
+      title,
+      views,
+      analytics,
+      thumbnail,
+      id
+    )
     .run();
 
   if (result.changes === 0) {
-    return { error: 'Entry not found' };
+    return { error: 'Record not found' };
   }
 
-  return { message: 'Entry updated successfully' };
+  return { message: 'Record updated successfully' };
 });
--- server/api/par/index.post.ts
+++ server/api/par/index.post.ts
@@ -1,11 +1,11 @@
 export default eventHandler(async (event) => {
-  const { type, plan, user, url, size, lastsync, secrets, pageid, title, views, analytics } = await readBody(event);
-  const db = hubDatabase();
+  const { type, plan, user, url, size, lastsync, secrets, pageid, title, views, analytics, thumbnail } = await readBody(event)
+  const db = hubDatabase()
 
   await db
-    .prepare('INSERT INTO par (type, plan, user, url, size, lastsync, secrets, pageid, title, views, analytics) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)')
-    .bind(type, plan, user, url, size, lastsync, secrets, pageid, title, views, analytics)
-    .run();
+    .prepare('INSERT INTO par (type, plan, user, url, size, lastsync, secrets, pageid, title, views, analytics, thumbnail) VALUES (?1, ?2, ?3, ?4, ?5, ?6, ?7, ?8, ?9, ?10, ?11, ?12)')
+    .bind(type, plan, user, url, size, lastsync, secrets, pageid, title, views, analytics, thumbnail)
+    .run()
 
-  return {};
-});
+  return {}
+})
