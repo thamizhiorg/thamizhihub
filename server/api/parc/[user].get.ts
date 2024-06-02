f730c1cd832f5534235825d022579b246061103e
tarfwrk <tarfwrk@gmail.com>
2024-06-02 14:44:56 +0000

    parc user 1

--- server/api/parc/[user].get.ts
+++ server/api/parc/[user].get.ts
@@ -1,8 +1,9 @@
 export default eventHandler(async (event) => {
-    const user = getRouterParam(event, 'user')
-    const db = hubDatabase()
-  
-    const message = await db.prepare('SELECT * FROM parc WHERE user = ?', ).bind(user).first()
+  const user = getRouterParam(event, 'user')
+  const db
+= hubDatabase()
 
-    return message
-  })
+  const message = await db.prepare('SELECT * FROM parc WHERE user = ?').bind(user).first()
+
+  return message
+})
