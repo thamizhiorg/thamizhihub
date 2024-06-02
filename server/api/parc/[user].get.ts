7e5d053ef66a4363a72f1bbea39e0c30ed4b4b92
tarfwrk <tarfwrk@gmail.com>
2024-06-02 14:49:57 +0000

    parc user 2

--- server/api/parc/[user].get.ts
+++ server/api/parc/[user].get.ts
@@ -1,9 +1,17 @@
 export default eventHandler(async (event) => {
-  const user = getRouterParam(event, 'user')
-  const db
-= hubDatabase()
+  const user = getRouterParam(event, 'user');
+  console.log("User from request:", user); 
 
-  const message = await db.prepare('SELECT * FROM parc WHERE user = ?').bind(user).first()
+  const db = hubDatabase();
 
-  return message
-})
+  const message = await db.prepare('SELECT * FROM parc WHERE user = ?').bind(user).first();
+
+  if (message) {
+      return message; 
+  } else {
+      return { 
+          statusCode: 404, 
+          body: JSON.stringify({ error: "No data found for this user" }) 
+      };
+  }
+});
