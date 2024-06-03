51c1122850ee7b82e5ea6d6143098ae020b065a8
tarfwrk <tarfwrk@gmail.com>
2024-06-03 11:37:14 +0000

    par email 3

--- server/api/par/[email].get.ts
+++ server/api/par/[email].get.ts
@@ -1,8 +1,11 @@
 export default eventHandler(async (event) => {
-  const email = getRouterParam(event, 'email')
-  const db = hubDatabase()
+  const email = getRouterParam(event, 'email');
+  const db = hubDatabase();
 
-  const message = await db.prepare('SELECT * FROM par WHERE email = ?', ).bind(email).all()
+  const result = await db.prepare('SELECT * FROM par WHERE email = ?', ).bind(email).all(); 
 
-  return message
-})
+  // Extract only the row data
+  const rowData = result.results; 
+
+  return rowData;
+});
