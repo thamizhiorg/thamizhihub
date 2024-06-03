6630934c1c4d8b88bee4ca7ef91db1fea1df2b5f
tarfwrk <tarfwrk@gmail.com>
2024-06-03 06:02:35 +0000

    par 1

--- server/api/par/index.get.ts
+++ server/api/par/index.get.ts
@@ -1,7 +1,7 @@
 export default eventHandler(async () => {
   const db = hubDatabase()
 
-  const { results } = await db.prepare('SELECT * FROM par ORDER BY created_at DESC').all()
+  const { results } = await db.prepare('SELECT * FROM par ORDER BY createdat DESC').all()
 
   return results
 })
