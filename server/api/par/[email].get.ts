7d775a8b361abc0bae493fcfcbc07490181626d7
tarfwrk <tarfwrk@gmail.com>
2024-06-03 11:32:47 +0000

    par email 2

--- server/api/par/[email].get.ts
+++ server/api/par/[email].get.ts
@@ -2,7 +2,7 @@ export default eventHandler(async (event) => {
   const email = getRouterParam(event, 'email')
   const db = hubDatabase()
 
-  const message = await db.prepare('SELECT * FROM par WHERE email = ?', ).bind(email).first()
+  const message = await db.prepare('SELECT * FROM par WHERE email = ?', ).bind(email).all()
 
   return message
 })
