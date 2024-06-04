ee5730527fa9008aaa9b80d08860dde5bec284f2
tarfwrk <tarfwrk@gmail.com>
2024-06-04 16:00:37 +0000

    parf update

--- server/api/parf/[id].get.ts
+++ server/api/parf/[id].get.ts
@@ -1,8 +1,8 @@
 export default eventHandler(async (event) => {
-  const id = getRouterParam(event, 'id')
-  const db = hubDatabase()
+    const id = getRouterParam(event, 'id')
+    const db = hubDatabase()
+  
+    const message = await db.prepare('SELECT * FROM parf WHERE id = ?', ).bind(id).first()
 
-  const message = await db.prepare('SELECT * FROM parf WHERE id = ?', ).bind(id).first()
-
-  return message
-})
+    return message
+  })
