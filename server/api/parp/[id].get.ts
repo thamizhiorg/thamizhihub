9e78f3cf5ade6072260fccd617ea3d97e8d411a2
tarfwrk <tarfwrk@gmail.com>
2024-06-04 15:43:58 +0000

    parp id 2

--- server/api/parp/[id].get.ts
+++ server/api/parp/[id].get.ts
@@ -1,11 +1,8 @@
-  export default eventHandler(async (event) => {
-    const id = getRouterParam(event, 'id');
-    const db = hubDatabase();
-  
-    const result = await db.prepare('SELECT * FROM parp WHERE id = ?', ).bind(id).all(); 
-  
-    // Extract only the row data
-    const rowData = result.results; 
-  
-    return rowData;
-  });
+export default eventHandler(async (event) => {
+  const id = getRouterParam(event, 'id')
+  const db = hubDatabase()
+
+  const message = await db.prepare('SELECT * FROM parp WHERE id = ?', ).bind(id).first()
+
+  return message
+})
